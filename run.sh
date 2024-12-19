b847d8ac40f2836acbfcda97f576cb62c17ae494
Levius_123 <88786955+levius-123@users.noreply.github.com>
2024-12-19 09:28:23 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 
 # Run both Python scripts in the background
 source venv/bin/activate &
-python3 /home/doma/Dokumenty/KOMPRESS/press3.html.py &
-python3 /home/doma/Dokumenty/KOMPRESS/raw.html.py &
+python3 /home/user/YOUR_DIRECTORY/press3.html.py &
+python3 /home/user/YOUR_DIRECOTRY/raw.html.py &
 sudo rm /mnt/konvert/* &
 ##########################################################
-#python3 /home/doma/Dokumenty/VIDEA/video.test9.html.py &
 
 # Wait for both processes to finish
 #wait
